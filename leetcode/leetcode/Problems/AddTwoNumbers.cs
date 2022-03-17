3b1c5410c33cbe404128ce498334d0ea01afd117
NePalDevelop <96785307+NePalDevelop@users.noreply.github.com>
2022-03-17 17:59:47 +0300

    v.1

---
+++ leetcode/leetcode/Problems/AddTwoNumbers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    static class Solution5
+    {
+        static public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            int num1, n = 0;
+            bool  first = true;
+
+            ListNode l, result = new();
+            l = result;
+
+            while ( l1 != null || l2 != null || n != 0)
+            {
+                if (first) first = false;
+                else
+                {
+                    l.next = new();
+                    l = l.next;
+                } 
+
+                int i = l1?.val ?? 0;
+                int j = l2?.val ?? 0;
+                num1 = i + j + n;
+                
+                if (num1 > 9)
+                {
+                    l.val = num1 - 10;
+                    n = 1;
+                }
+                else
+                {
+                    l.val = num1;
+                    n = 0;
+                }
+
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+            return result;
+        }
+    }
+    
+    public class ListNode
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int val = 0, ListNode next = null)
+      {
+         this.val = val;
+         this.next = next;
+      }
+  }
+}
---
+++ leetcode/leetcode/Problems/LongestString.cs
@@ -0,0 +1,24 @@
+﻿namespace Solutions
+{
+    static class LongestString
+    {
+        static public int LengthOfLongestSubstring(string s)
+        {
+            string workSubString = string.Empty,
+             longestString = string.Empty;
+
+            for (var i = 0; i<s.Length;i++)
+            {
+                if (workSubString.Contains(s[i])) {
+                    i = i - workSubString.Length;
+                    workSubString = string.Empty;
+                } else
+                {
+                    workSubString = workSubString + s[i];
+                }
+                if (longestString.Length < workSubString.Length) longestString = workSubString;
+            }
+            return longestString.Length;
+        }
+    }
+}
